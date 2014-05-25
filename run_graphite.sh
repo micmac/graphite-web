2ba813104665c52559b7972e0fdeeda80d8edd76
Endre Hirling <endre.hirling@prezi.com>
2014-05-25 20:37:24 +0200

    Set some saner defaults for config and storage path

--- run_graphite.sh
+++ run_graphite.sh
@@ -3,4 +3,18 @@
 cd $(dirname $0)
 . virtualenv/bin/activate
 cd webapp
+
+DEFAULTS_FILE=/etc/default/graphite-web
+GRAPHITE_CONF_DIR=/opt/graphite/conf
+GRAPHITE_STORAGE_DIR=/opt/graphite/storage
+GRAPHITE_LOCAL_SETTINGS="$GRAPHITE_CONF_DIR/local_settings.py"
+if [ -r $DEFAULTS_FILE ]; then
+  .  $DEFAULTS_FILE
+fi
+export ${!GRAPHITE_*}
+
+mkdir -p "${GRAPHITE_STORAGE_DIR}/log/webapp" "$GRAPHITE_CONF_DIR"
+
+ln -sf "$GRAPHITE_LOCAL_SETTINGS" webapp/graphite/local_settings.py
+
 exec gunicorn --bind 0.0.0.0:8000 graphite.wsgi
